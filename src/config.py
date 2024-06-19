f84315d2c45fd517f6a6ac93fa8df721e0187065
Akshay B <akshayb1389@gmail.com>
2024-06-19 09:26:41 +0000

    feat: enhance application features

---
+++ src/config.py
@@ -0,0 +1,19 @@
+DIR_SERVER_PORT = 5002
+CACHE_SERVER_PORT = 5005
+LOCK_SERVER_PORT = 5006
+AUTH_SERVER_PORT = 5007
+REGISTRY_SERVER_PORT = 5010
+CACHE_FILE_CAPACITY = 3
+CACHE_FILE_PATH = '/home/lavelld/Documents/SS/Internet_Apps/DFS/src/cache'
+JSON_HEADER = {'content-type': 'application/json'}
+ADMIN_DETAILS = {
+    'username': 'lavelld',
+    'password': 'plavelld',
+    'privilege': 'admin'
+}
+
+server_port_1=6015
+server_port_2=6016
+server_port_3=6017
+server_port_4=6018
+#Note:: this exists in conig.cfg file aswell
